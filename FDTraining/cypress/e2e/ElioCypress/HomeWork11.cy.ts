eb88dfea28498256997550d8f5ec51471409c8cb
Alia224 <azarelio224@outlook.com>
2025-11-25 13:58:01 +0200

    test

--- FDTraining/cypress/e2e/ElioCypress/HomeWork11.cy.ts
+++ FDTraining/cypress/e2e/ElioCypress/HomeWork11.cy.ts
@@ -5,5 +5,6 @@ describe(`using xpath`,()=>it(`using xpath`,()=>{
     cy.visit(`https://hrm.neotechacademy.com/auth/login`)
     cy.xpath(`//input[@id='txtUsername']`).type(`Admin`)
     cy.wait(2000)
+    console.log(`Elio Cypress Login Page Loaded`)
 
 }))
